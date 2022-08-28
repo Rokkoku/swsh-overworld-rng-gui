7f046c6c6d15f76e3c694ae42369c1053ce23f3d
Rokkoku <78675296+Rokkoku@users.noreply.github.com>
2022-08-28 18:10:53 +0900

    Update Loto-ID.Designer.cs

--- SWSH_OWRNG_Generator.WinForms/Subforms/Loto-ID/Loto-ID.Designer.cs
+++ SWSH_OWRNG_Generator.WinForms/Subforms/Loto-ID/Loto-ID.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SWSH_OWRNG_Generator.WinForms
+namespace SWSH_OWRNG_Generator.WinForms
 {
     partial class Loto_ID
     {
@@ -63,33 +63,33 @@
             // LotoIDProgressBar
             // 
             this.LotoIDProgressBar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.LotoIDProgressBar.Location = new System.Drawing.Point(14, 182);
+            this.LotoIDProgressBar.Location = new System.Drawing.Point(29, 182);
             this.LotoIDProgressBar.Name = "LotoIDProgressBar";
             this.LotoIDProgressBar.Size = new System.Drawing.Size(243, 10);
             this.LotoIDProgressBar.TabIndex = 157;
             // 
             // LotoIDSearch
             // 
-            this.LotoIDSearch.Location = new System.Drawing.Point(12, 145);
+            this.LotoIDSearch.Location = new System.Drawing.Point(27, 145);
             this.LotoIDSearch.Name = "LotoIDSearch";
             this.LotoIDSearch.Size = new System.Drawing.Size(245, 31);
             this.LotoIDSearch.TabIndex = 6;
-            this.LotoIDSearch.Text = "Search!";
+            this.LotoIDSearch.Text = "検索!";
             this.LotoIDSearch.UseVisualStyleBackColor = true;
             this.LotoIDSearch.Click += new System.EventHandler(this.LotoIDSearch_Click);
             // 
             // LabelInitialAdv
             // 
             this.LabelInitialAdv.AutoSize = true;
-            this.LabelInitialAdv.Location = new System.Drawing.Point(5, 93);
+            this.LabelInitialAdv.Location = new System.Drawing.Point(7, 93);
             this.LabelInitialAdv.Name = "LabelInitialAdv";
-            this.LabelInitialAdv.Size = new System.Drawing.Size(57, 15);
+            this.LabelInitialAdv.Size = new System.Drawing.Size(70, 15);
             this.LabelInitialAdv.TabIndex = 156;
-            this.LabelInitialAdv.Text = "Init. Adv.:";
+            this.LabelInitialAdv.Text = "初期消費数:";
             // 
             // InputInitialAdv
             // 
-            this.InputInitialAdv.Location = new System.Drawing.Point(68, 90);
+            this.InputInitialAdv.Location = new System.Drawing.Point(83, 90);
             this.InputInitialAdv.MaxLength = 13;
             this.InputInitialAdv.Name = "InputInitialAdv";
             this.InputInitialAdv.Size = new System.Drawing.Size(189, 23);
@@ -99,7 +99,7 @@
             // LabelMaxAdv
             // 
             this.LabelMaxAdv.AutoSize = true;
-            this.LabelMaxAdv.Location = new System.Drawing.Point(47, 119);
+            this.LabelMaxAdv.Location = new System.Drawing.Point(62, 119);
             this.LabelMaxAdv.Name = "LabelMaxAdv";
             this.LabelMaxAdv.Size = new System.Drawing.Size(15, 15);
             this.LabelMaxAdv.TabIndex = 155;
@@ -107,7 +107,7 @@
             // 
             // InputMaxAdv
             // 
-            this.InputMaxAdv.Location = new System.Drawing.Point(68, 116);
+            this.InputMaxAdv.Location = new System.Drawing.Point(83, 116);
             this.InputMaxAdv.MaxLength = 13;
             this.InputMaxAdv.Name = "InputMaxAdv";
             this.InputMaxAdv.Size = new System.Drawing.Size(189, 23);
@@ -119,24 +119,24 @@
             this.LotoIdResults.AllowUserToAddRows = false;
             this.LotoIdResults.AllowUserToDeleteRows = false;
             this.LotoIdResults.AllowUserToResizeRows = false;
-            this.LotoIdResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.LotoIdResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LotoIdResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.LotoIdResults.Location = new System.Drawing.Point(263, 12);
+            this.LotoIdResults.Location = new System.Drawing.Point(306, 12);
             this.LotoIdResults.MultiSelect = false;
             this.LotoIdResults.Name = "LotoIdResults";
             this.LotoIdResults.ReadOnly = true;
             this.LotoIdResults.RowHeadersVisible = false;
             this.LotoIdResults.RowTemplate.Height = 25;
             this.LotoIdResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.LotoIdResults.Size = new System.Drawing.Size(627, 406);
+            this.LotoIdResults.Size = new System.Drawing.Size(584, 406);
             this.LotoIdResults.TabIndex = 154;
             // 
             // LabelDisplayIDInput
             // 
             this.LabelDisplayIDInput.AutoSize = true;
-            this.LabelDisplayIDInput.Location = new System.Drawing.Point(41, 67);
+            this.LabelDisplayIDInput.Location = new System.Drawing.Point(56, 67);
             this.LabelDisplayIDInput.Name = "LabelDisplayIDInput";
             this.LabelDisplayIDInput.Size = new System.Drawing.Size(21, 15);
             this.LabelDisplayIDInput.TabIndex = 153;
@@ -145,7 +145,7 @@
             // LabelState1
             // 
             this.LabelState1.AutoSize = true;
-            this.LabelState1.Location = new System.Drawing.Point(12, 42);
+            this.LabelState1.Location = new System.Drawing.Point(27, 42);
             this.LabelState1.Name = "LabelState1";
             this.LabelState1.Size = new System.Drawing.Size(50, 15);
             this.LabelState1.TabIndex = 151;
@@ -153,7 +153,7 @@
             // 
             // InputState1
             // 
-            this.InputState1.Location = new System.Drawing.Point(68, 38);
+            this.InputState1.Location = new System.Drawing.Point(83, 38);
             this.InputState1.MaxLength = 16;
             this.InputState1.Name = "InputState1";
             this.InputState1.Size = new System.Drawing.Size(189, 23);
@@ -161,7 +161,7 @@
             // 
             // InputState0
             // 
-            this.InputState0.Location = new System.Drawing.Point(68, 12);
+            this.InputState0.Location = new System.Drawing.Point(83, 12);
             this.InputState0.MaxLength = 16;
             this.InputState0.Name = "InputState0";
             this.InputState0.Size = new System.Drawing.Size(189, 23);
@@ -170,7 +170,7 @@
             // LabelState0
             // 
             this.LabelState0.AutoSize = true;
-            this.LabelState0.Location = new System.Drawing.Point(12, 15);
+            this.LabelState0.Location = new System.Drawing.Point(27, 15);
             this.LabelState0.Name = "LabelState0";
             this.LabelState0.Size = new System.Drawing.Size(53, 15);
             this.LabelState0.TabIndex = 149;
@@ -178,7 +178,7 @@
             // 
             // InputDisplayID
             // 
-            this.InputDisplayID.Location = new System.Drawing.Point(68, 64);
+            this.InputDisplayID.Location = new System.Drawing.Point(83, 64);
             this.InputDisplayID.MaxLength = 6;
             this.InputDisplayID.Name = "InputDisplayID";
             this.InputDisplayID.Size = new System.Drawing.Size(189, 23);
@@ -187,15 +187,15 @@
             // LabelLoadedFromFile
             // 
             this.LabelLoadedFromFile.AutoSize = true;
-            this.LabelLoadedFromFile.Location = new System.Drawing.Point(12, 203);
+            this.LabelLoadedFromFile.Location = new System.Drawing.Point(15, 202);
             this.LabelLoadedFromFile.Name = "LabelLoadedFromFile";
-            this.LabelLoadedFromFile.Size = new System.Drawing.Size(116, 15);
+            this.LabelLoadedFromFile.Size = new System.Drawing.Size(128, 15);
             this.LabelLoadedFromFile.TabIndex = 159;
-            this.LabelLoadedFromFile.Text = "Loaded IDs from file:";
+            this.LabelLoadedFromFile.Text = "ファイルからIDを読み込む:";
             // 
             // ButtonReload
             // 
-            this.ButtonReload.Location = new System.Drawing.Point(206, 198);
+            this.ButtonReload.Location = new System.Drawing.Point(221, 198);
             this.ButtonReload.Name = "ButtonReload";
             this.ButtonReload.Size = new System.Drawing.Size(51, 25);
             this.ButtonReload.TabIndex = 7;
@@ -205,7 +205,7 @@
             // 
             // InputLoadedFromFile
             // 
-            this.InputLoadedFromFile.Location = new System.Drawing.Point(134, 199);
+            this.InputLoadedFromFile.Location = new System.Drawing.Point(149, 199);
             this.InputLoadedFromFile.MaxLength = 16;
             this.InputLoadedFromFile.Name = "InputLoadedFromFile";
             this.InputLoadedFromFile.ReadOnly = true;
@@ -218,76 +218,76 @@
             this.CheckMaster.AutoSize = true;
             this.CheckMaster.Checked = true;
             this.CheckMaster.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckMaster.Location = new System.Drawing.Point(12, 398);
+            this.CheckMaster.Location = new System.Drawing.Point(29, 274);
             this.CheckMaster.Name = "CheckMaster";
-            this.CheckMaster.Size = new System.Drawing.Size(116, 19);
+            this.CheckMaster.Size = new System.Drawing.Size(177, 19);
             this.CheckMaster.TabIndex = 15;
-            this.CheckMaster.Text = "Show Master Ball";
+            this.CheckMaster.Text = "特賞「マスターボール」を表示する";
             this.CheckMaster.UseVisualStyleBackColor = true;
             // 
             // CheckRareCandy
             // 
             this.CheckRareCandy.AutoSize = true;
-            this.CheckRareCandy.Location = new System.Drawing.Point(12, 373);
+            this.CheckRareCandy.Location = new System.Drawing.Point(29, 299);
             this.CheckRareCandy.Name = "CheckRareCandy";
-            this.CheckRareCandy.Size = new System.Drawing.Size(118, 19);
+            this.CheckRareCandy.Size = new System.Drawing.Size(163, 19);
             this.CheckRareCandy.TabIndex = 14;
-            this.CheckRareCandy.Text = "Show Rare Candy";
+            this.CheckRareCandy.Text = "1等「ふしぎなアメ」を表示する";
             this.CheckRareCandy.UseVisualStyleBackColor = true;
             // 
             // CheckPPMax
             // 
             this.CheckPPMax.AutoSize = true;
-            this.CheckPPMax.Location = new System.Drawing.Point(12, 348);
+            this.CheckPPMax.Location = new System.Drawing.Point(29, 324);
             this.CheckPPMax.Name = "CheckPPMax";
-            this.CheckPPMax.Size = new System.Drawing.Size(98, 19);
+            this.CheckPPMax.Size = new System.Drawing.Size(179, 19);
             this.CheckPPMax.TabIndex = 13;
-            this.CheckPPMax.Text = "Show PP Max";
+            this.CheckPPMax.Text = "2等「ポイントマックス」を表示する";
             this.CheckPPMax.UseVisualStyleBackColor = true;
             // 
             // CheckPPUp
             // 
             this.CheckPPUp.AutoSize = true;
-            this.CheckPPUp.Location = new System.Drawing.Point(12, 323);
+            this.CheckPPUp.Location = new System.Drawing.Point(29, 349);
             this.CheckPPUp.Name = "CheckPPUp";
-            this.CheckPPUp.Size = new System.Drawing.Size(90, 19);
+            this.CheckPPUp.Size = new System.Drawing.Size(170, 19);
             this.CheckPPUp.TabIndex = 12;
-            this.CheckPPUp.Text = "Show PP Up";
+            this.CheckPPUp.Text = "3等「ポイントアップ」を表示する";
             this.CheckPPUp.UseVisualStyleBackColor = true;
             // 
             // CheckMooMoo
             // 
             this.CheckMooMoo.AutoSize = true;
-            this.CheckMooMoo.Location = new System.Drawing.Point(12, 298);
+            this.CheckMooMoo.Location = new System.Drawing.Point(29, 374);
             this.CheckMooMoo.Name = "CheckMooMoo";
-            this.CheckMooMoo.Size = new System.Drawing.Size(134, 19);
+            this.CheckMooMoo.Size = new System.Drawing.Size(168, 19);
             this.CheckMooMoo.TabIndex = 11;
-            this.CheckMooMoo.Text = "Show MooMoo Milk";
+            this.CheckMooMoo.Text = "4等「モーモーミルク」を表示する";
             this.CheckMooMoo.UseVisualStyleBackColor = true;
             // 
             // CheckNoItem
             // 
             this.CheckNoItem.AutoSize = true;
-            this.CheckNoItem.Location = new System.Drawing.Point(12, 273);
+            this.CheckNoItem.Location = new System.Drawing.Point(29, 399);
             this.CheckNoItem.Name = "CheckNoItem";
-            this.CheckNoItem.Size = new System.Drawing.Size(101, 19);
+            this.CheckNoItem.Size = new System.Drawing.Size(145, 19);
             this.CheckNoItem.TabIndex = 10;
-            this.CheckNoItem.Text = "Show No Item";
+            this.CheckNoItem.Text = "「全部はずれ」を表示する";
             this.CheckNoItem.UseVisualStyleBackColor = true;
             // 
             // LabelNPCs
             // 
             this.LabelNPCs.AutoSize = true;
-            this.LabelNPCs.Location = new System.Drawing.Point(23, 232);
+            this.LabelNPCs.Location = new System.Drawing.Point(38, 232);
             this.LabelNPCs.Name = "LabelNPCs";
-            this.LabelNPCs.Size = new System.Drawing.Size(39, 15);
+            this.LabelNPCs.Size = new System.Drawing.Size(33, 15);
             this.LabelNPCs.TabIndex = 169;
-            this.LabelNPCs.Text = "NPCs:";
+            this.LabelNPCs.Text = "NPC:";
             // 
             // InputNPCs
             // 
             this.InputNPCs.Enabled = false;
-            this.InputNPCs.Location = new System.Drawing.Point(68, 229);
+            this.InputNPCs.Location = new System.Drawing.Point(83, 229);
             this.InputNPCs.MaxLength = 2;
             this.InputNPCs.Name = "InputNPCs";
             this.InputNPCs.Size = new System.Drawing.Size(34, 23);
@@ -297,11 +297,11 @@
             // CheckMenuClose
             // 
             this.CheckMenuClose.AutoSize = true;
-            this.CheckMenuClose.Location = new System.Drawing.Point(108, 232);
+            this.CheckMenuClose.Location = new System.Drawing.Point(123, 232);
             this.CheckMenuClose.Name = "CheckMenuClose";
-            this.CheckMenuClose.Size = new System.Drawing.Size(139, 19);
+            this.CheckMenuClose.Size = new System.Drawing.Size(154, 19);
             this.CheckMenuClose.TabIndex = 9;
-            this.CheckMenuClose.Text = "Consider Menu Close";
+            this.CheckMenuClose.Text = "メニュー閉じを考慮に入れる";
             this.CheckMenuClose.UseVisualStyleBackColor = true;
             this.CheckMenuClose.CheckedChanged += new System.EventHandler(this.CheckMenuClose_CheckedChanged);
             // 
@@ -337,7 +337,7 @@
             this.Controls.Add(this.LabelState0);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Loto_ID";
-            this.Text = "Loto-ID";
+            this.Text = "IDくじ";
             ((System.ComponentModel.ISupportInitialize)(this.LotoIdResults)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.LotoIDBindingSource)).EndInit();
             this.ResumeLayout(false);
